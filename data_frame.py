2cc3712103ca5bdb0eec98506adfde3cf7f2fee3
MANI <abdulrehman9745@outlook.com>
2025-01-03 19:06:26 +0500

    worksheets and data updated

--- data_frame.py
+++ data_frame.py
@@ -12,7 +12,7 @@ user_data = st.connection("gsheets_userData", type=GSheetsConnection)
 # Function to return the dataframe to other pages
 def BusinessDataFrame():
     # df = pd.read_csv("business_df_after_more_clean.csv") 
-    df = business_data.read(worksheet="business_df_after_more_clean")
+    df = business_data.read(worksheet="business")
     return df
 
 def ReviewsDataFrame():
@@ -30,14 +30,14 @@ def UsersDataFrame():
 def UserProfiles():
     
     # fetch existing data
-    df = user_profiles.read(worksheet="random_user_profiles_data")
+    df = user_profiles.read(worksheet="user_profile")
     # df = pd.read_csv("random_user_profiles_data.csv")
     return df
 
 def CreateProfile(new_user):
 
     updated = new_user
-    user_profiles.update(worksheet="random_user_profiles_data", data=updated)
+    user_profiles.update(worksheet="user_profile", data=updated)
     # updated.to_csv("random_user_profiles_data.csv", index=False)
     return True
 
@@ -45,7 +45,7 @@ def UpdateProfile(updated_user):
 
     updated = updated_user
     # updated.to_csv("random_user_profiles_data.csv", index=False)
-    user_profiles.update(worksheet="random_user_profiles_data", data=updated)
+    user_profiles.update(worksheet="user_profile", data=updated)
     st.cache_data.clear()
     return True
 
@@ -53,17 +53,17 @@ def UpdateBusiness(updated_business):
     
     updatd = updated_business
     # updatd.to_csv("business_df_after_more_clean.csv", index=False)
-    business_data.update(worksheet="business_df_after_more_clean", data=updatd)    
+    business_data.update(worksheet="business", data=updatd)    
     return True
 
 
 def DeleteProfile(email):
 
-    df = user_profiles.read(worksheet="random_user_profiles_data")
+    df = user_profiles.read(worksheet="user_profile")
     df = df[df['Email'] != email]
     # df.to_csv("random_user_profiles_data.csv", index=False)
 
-    user_profiles.update(worksheet="random_user_profiles_data", data=df)
+    user_profiles.update(worksheet="user_profile", data=df)
     
     st.cache_data.clear()
     
